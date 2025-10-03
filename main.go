5ac412f3ad7db1c0df2803a7415ee146428a5b60
r3g492 <kangggunw3492@gmail.com>
2025-10-04 01:56:09 +0900

    init 3d

--- main.go
+++ main.go
@@ -3,16 +3,48 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	var width int32 = 800
+	var height int32 = 450
+
+	rl.InitWindow(width, height, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
+
+	var camera = rl.Camera3D{
+		Position: rl.Vector3{
+			X: 0,
+			Y: 1,
+			Z: 0,
+		},
+		Target: rl.Vector3{
+			X: 1,
+			Y: 0,
+			Z: 0,
+		},
+		Up: rl.Vector3{
+			X: 0,
+			Y: 1,
+			Z: 0,
+		},
+		Fovy:       90,
+		Projection: rl.CameraPerspective,
+	}
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.BeginMode3D(camera)
+
+		rl.DrawGrid(
+			1024,
+			1,
+		)
+
+		rl.EndMode3D()
 
 		rl.EndDrawing()
 	}
